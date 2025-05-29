f424ee801a12b2b7dd68ee91b3ccf9ef64768967
Arianna Diaz <aldiaz0223@gmail.com>
2025-05-28 23:00:18 -0400

    Add Fireball damage to player and remove unused asset

--- Assets/Scripts/FireBall.cs
+++ Assets/Scripts/FireBall.cs
@@ -4,11 +4,22 @@ public class FireBall : MonoBehaviour
 {
 
     [SerializeField] private float shootImpulse;
+    [SerializeField] private float playerDamege;
     private Rigidbody2D rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.AddForce(transform.right * shootImpulse, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("PlayerHitBox"))
+        {
+            HealthSystem healthSystem = other.gameObject.GetComponent<HealthSystem>();
+            healthSystem.GetDamage(playerDamege);
+            gameObject.SetActive(false);
+        }
     }
 }
